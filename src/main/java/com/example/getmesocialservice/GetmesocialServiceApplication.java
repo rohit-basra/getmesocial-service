9e60acac8f9b56726487463f8e69cc02de8805d1
rohit-basra <basra.rohit@gmail.com>
2021-12-13 23:45:58 -0500

    Commit-2

--- src/main/java/com/example/getmesocialservice/GetmesocialServiceApplication.java
+++ src/main/java/com/example/getmesocialservice/GetmesocialServiceApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GetmesocialServiceApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(GetmesocialServiceApplication.class, args);
 	}
+
 
 }
--- src/main/java/com/example/getmesocialservice/resource/UserResource.java
+++ src/main/java/com/example/getmesocialservice/resource/UserResource.java
@@ -1,2 +1,18 @@
-package com.example.getmesocialservice.resource;public class UserResource {
+package com.example.getmesocialservice.resource;
+
+import com.example.getmesocialservice.model.User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class UserResource {
+
+    @GetMapping("/user")
+    public User getUser(){
+        User user = new User("rohit","233 xyz",35,"");
+        return user;
+
+    }
 }
